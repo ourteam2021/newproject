8b0cc4ff39e691c734ad076ca9f38288a6d3140f
rafeeek <refoo@outlook.com>
2020-12-26 20:38:28 +0200

    last

--- js/rafeekApp.js
+++ js/rafeekApp.js
@@ -20,17 +20,29 @@ let firstcolor = document.getElementById("firstcolor"),
     seccolor = document.getElementById("seccolor"),
     thirdcolor = document.getElementById("thirdcolor"),
     fourcolor = document.getElementById("fourcolor"),
-    fivecolor = document.getElementById("fivecolor")
+    fivecolor = document.getElementById("fivecolor"),
+    firsttext = document.getElementById("firsttext"),
+    sectext = document.getElementById("sectext"),
+    thirdtext = document.getElementById("thirdtext"),
+    fourtext = document.getElementById("fourtext"),
+    fivetext = document.getElementById("fivetext")
 
+    
 async function onthercol() {
     let x = await fetch("https://www.thecolorapi.com/scheme?hex="+hasala)  
     let z = await x.json()
     let color = z.colors
+    
     firstcolor.style.backgroundColor = color[0].hex.value
     seccolor.style.backgroundColor = color[1].hex.value
     thirdcolor.style.backgroundColor = color[2].hex.value
     fourcolor.style.backgroundColor = color[3].hex.value
     fivecolor.style.backgroundColor = color[4].hex.value
+    firsttext.innerHTML = color[0].hex.value
+    sectext.innerHTML = color[1].hex.value
+    thirdtext.innerHTML = color[2].hex.value
+    fourtext.innerHTML = color[3].hex.value
+    fivetext.innerHTML = color[4].hex.value
 }
 
 
