35fdd308c8386e2a1bc07b7ec7e6c94462dbefd5
rafeeek <refoo@outlook.com>
2020-12-28 00:33:27 +0200

    error fixed

--- js/shrouk.js
+++ js/shrouk.js
@@ -1,11 +1,12 @@
 const btns = document.querySelectorAll(".btn");
-const screen = document.querySelectorAll(".screen");
+const screen = document.querySelector(".screen");
 const equalBtn = document.querySelector(".btn-equal");
 const clearBtn = document.querySelector(".btn-clear");
 
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
     let number = btns[i].getAttribute("data-num");
+
     screen.value += number
   })
 }
