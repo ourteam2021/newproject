e2e76ba1a950c809084f96953737e04b18b72599
shrouk-zaiDa <76027466+shrouk-zaiDa@users.noreply.github.com>
2020-12-28 00:13:59 +0200

    changes

--- js/shrouk.js
+++ js/shrouk.js
@@ -1,30 +1,25 @@
-alert("j");
-
 const btns = document.querySelectorAll(".btn");
 const screen = document.querySelectorAll(".screen");
-const equalBtn = document.querySelectorAll(".btn-equal");
-const clearBtn = document.querySelectorAll(".btn-clear");
+const equalBtn = document.querySelector(".btn-equal");
+const clearBtn = document.querySelector(".btn-clear");
 
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
     let number = btns[i].getAttribute("data-num");
-    screen.value += number;
+    screen.value += number
   })
 }
 
 equalBtn.addEventListener("click", function () {
-  if (screen.value === "") {
+  if(screen.value === "") {
     alert("input is empty");
   } 
   else {
-    let value = eval(screen.value);
-    screen.value = value;
+    let value = eval(screen.value)
+    screen.value = value
   }
-
-  let value = eval(screen.value);
-  screen.value = value;
 })
 
 clearBtn.addEventListener("click", function () {
-  screen.value = "";
-});
+    screen.value = "";
+  })
